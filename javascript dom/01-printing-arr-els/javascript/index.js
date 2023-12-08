f12eb4cacb34af18f4ca3d9678b184d3aea221e6
kajasharif31 <kajasharif366@gmail.com>
2023-12-08 15:45:08 +0530

    Dom task 01 printing arr el in a sentence

---
+++ javascript dom/01-printing-arr-els/javascript/index.js
@@ -0,0 +1,46 @@
+// Task : printing each list to a single line paragraph
+// Task : dynamic styleing in dom
+
+// Dom elements
+const skillsEls = document.querySelectorAll(".skills");
+const skillInputEl = document.getElementById("skillInput");
+const paragraphEls = document.getElementsByTagName("p");
+const submitEl = document.getElementById("submit");
+
+// events
+submitEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  paragraphEls[0].innerText = "I have some Knowledge in :";
+
+  // console.log(skillsEls[0].childNodes);
+  const skillsArr = [];
+  skillsEls[0].childNodes.forEach((element) => {
+    if (element.tagName === "LI") {
+      skillsArr.push(element.innerText);
+    }
+  });
+  skillsArr.forEach((element, index) => {
+    index !== skillsArr.length - 1
+      ? // += addition increment &  concatenation
+        (paragraphEls[0].innerText += ` ${element},`)
+      : (paragraphEls[0].innerText += ` ${element} etc...`);
+  });
+
+  // ui style
+  paragraphEls[0].classList.remove("d-none");
+  paragraphEls[0].style.display = "block";
+  // paragraphEls[0].classList.add("d-block");
+});
+
+skillInputEl.addEventListener("keypress", (e) => {
+  if (e.code !== "Enter") {
+    return;
+  }
+  const skill = skillInputEl.value.trim();
+  const liEl = document.createElement("li");
+  liEl.textContent = skill;
+
+  skillsEls[0].appendChild(liEl);
+  skillInputEl.value = "";
+  e.preventDefault();
+});
