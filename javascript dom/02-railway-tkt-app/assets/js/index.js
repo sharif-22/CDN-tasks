3348f57ce536321425084c50ecd61c0dd1301e05
kajasharif31 <kajasharif366@gmail.com>
2023-12-20 21:27:33 +0530

    feat: html validation and inputs append feature with new variables

--- javascript dom/02-railway-tkt-app/assets/js/index.js
+++ javascript dom/02-railway-tkt-app/assets/js/index.js
@@ -1,88 +1,110 @@
 const formEl = document.forms.ticketForm;
 
-const addMemberEl = document.querySelector("#addMember");
+const addPassengersBtnEl = document.querySelector("#addPassengers");
 const passengersListEl = document.querySelector("#passengerList");
 const passengersEls = document.querySelectorAll(".passenger");
 
-addMemberEl.addEventListener("click", (e) => {
-  // preventing default behaviour
+addPassengersBtnEl.addEventListener("click", (e) => {
   e.preventDefault();
   let childLength = passengersListEl.children.length + 1;
 
   // appending html el
-  let addMemberHTML = `
-  <!-- passengers  -->
-  <div>
-    <div class="passenger">
-      <!-- name field -->
-      <div class="flex flex-col gap-3">
-        <label for="passenger${childLength}Name">Enter Passenger ${childLength} Name </label>
-        <input
-          class="passenger p-2"
-          type="text"
-          name="passenger${childLength}Name"
-          id="passenger${childLength}Name"
-          placeholder="Enter Passenger Name "
-        />
-      </div>
-      <!-- gender radio  -->
-      <div class="flex justify-between p-3">
-        <div>
-          <input
-            type="radio"
-            name="passenger${childLength}Gender"
-            id="male${childLength}"
-            value="male"
-          />
-          <label for="male${childLength}">Male </label>
-        </div>
+  const divEl = document.createElement("div");
 
-        <div>
-          <input
-            type="radio"
-            name="passenger${childLength}Gender"
-            id="female${childLength}"
-            value="female"
-          />
-          <label for="female${childLength}">Female </label>
-        </div>
-
-        <div>
-          <input
-            type="radio"
-            name="passenger${childLength}Gender"
-            id="others${childLength}"
-            value="others"
-          />
-          <label for="others${childLength}">Others </label>
-        </div>
-      </div>
-      <!-- age field -->
-      <div class="flex flex-col gap-3">
-        <label for="passenger${childLength}Age">Enter your Age </label>
-        <input
-          class="p-2"
-          type="number"
-          name="passenger${childLength}Age"
-          id="passenger${childLength}Age"
-          placeholder="Enter your Age "
-        />
-      </div>
-    </div>
-    <hr class="my-3" />
-  </div>
-  `;
+  const nameEl = createInput(
+    childLength,
+    `passenr${childLength}Name`,
+    "text",
+    `Enter passenger${childLength} Name`
+  );
+  const gender = ["Male", "Female", "Others"];
+  const ageEl = createInput(
+    childLength,
+    `passenger${childLength}Age`,
+    "number",
+    `Enter passenger${childLength} Age`
+  );
 
   // members upto 5 only
   if (childLength < 6) {
-    passengersListEl.innerHTML += addMemberHTML;
+    divEl.append(nameEl, radioCollection(gender, childLength), ageEl);
   } else {
     console.log("tickes solds upto 5 members only ");
+  }
+  passengersListEl.append(divEl);
+
+  function createInput(count, passengerData, inputType, labelTxt) {
+    // create el
+    const divEl = document.createElement("div");
+    const labelEl = document.createElement("label");
+    const inputEl = document.createElement("input");
+
+    // add classes
+    divEl.classList.add("flex", "flex-col", "gap-3");
+    inputEl.classList.add("passenger", "p-2");
+
+    // setting attributes
+    if (inputType == "number" && passengerData == `passenger${count}Age`) {
+      inputEl.setAttribute("min", 0);
+      inputEl.setAttribute("max", 110);
+    }
+
+    labelEl.setAttribute("for", passengerData);
+
+    inputEl.setAttribute("type", inputType);
+    inputEl.setAttribute("id", passengerData);
+    inputEl.setAttribute("name", passengerData);
+    inputEl.setAttribute("placeholder", `Enter Passenger ${count} Age `);
+    inputEl.setAttribute("required", "required");
+
+    labelEl.innerText = labelTxt;
+
+    //   appending
+    divEl.append(labelEl, inputEl);
+
+    return divEl;
+  }
+
+  function radioCollection(arr, count) {
+    const divEl = document.createElement("div");
+
+    divEl.classList.add("flex", "justify-between", "p-3");
+
+    for (let index = 0; index < arr.length; index++) {
+      divEl.appendChild(
+        createRadioButton(count, arr[index], arr[index], arr[index])
+      );
+    }
+    return divEl;
+  }
+
+  function createRadioButton(count, id, value, labelText) {
+    let name = `passenger${count}Gender`;
+
+    var radioDiv = document.createElement("div");
+
+    var input = document.createElement("input");
+    input.setAttribute("type", "radio");
+    input.setAttribute("name", name);
+    input.setAttribute("id", `${id.toLowerCase()}${count}`);
+    input.setAttribute("value", value.toLowerCase());
+    input.setAttribute("required", "required");
+
+    input.style.margin = "6px";
+
+    var label = document.createElement("label");
+    label.setAttribute("for", `${id.toLowerCase()}${count}`);
+    label.textContent = labelText;
+
+    radioDiv.append(input, label);
+
+    return radioDiv;
   }
 });
 
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
+
   const formData = new FormData(formEl);
 
   // console.log(Object.fromEntries(formData)); // return all data in unstructure way
@@ -108,12 +130,13 @@ formEl.addEventListener("submit", (event) => {
     toStation: formDataObj["toStation"],
     fromStation: formDataObj["fromStation"],
     dateOfJourney: formDataObj["dateOfJourney"],
-    totalPassengers: passengersObjsArr,
+    passengersDetails: passengersObjsArr,
   };
+
   // console.log(journeyData); // structured data for appending to  server
   // converting journeyDate -> JSON
   const jsonData = JSON.stringify(journeyData);
-  // console.log("JSON BODY", jsonData);
+  console.log("JSON BODY", jsonData);
 
   // Send to Backend
   const url = "http://localhost:3000/tickets";
@@ -128,6 +151,7 @@ formEl.addEventListener("submit", (event) => {
     });
   };
   postMethod(jsonData, url);
+
   // removing existing inputs
-  formEl.reset();
+  // formEl.reset();
 });
