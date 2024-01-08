1dffc1d620e01f48cf750ab6b69e24e33ec188bd
kajasharif31 <kajasharif366@gmail.com>
2024-01-08 19:00:15 +0530

    intial-setup: yt thumbnail downloader

---
+++ javascript/02-javascript-dom/07-youtube-thumbnail-downloader-app/assets/js/main.js
@@ -0,0 +1,1 @@
+document.body.classList.add("bg-red-500");
---
+++ javascript/02-javascript-dom/07-youtube-thumbnail-downloader-app/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./assets/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
