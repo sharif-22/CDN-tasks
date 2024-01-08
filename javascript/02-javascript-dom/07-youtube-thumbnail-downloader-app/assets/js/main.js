5960538d0e38810ee299ac2aecbf5cfd4fe50251
kajasharif31 <kajasharif366@gmail.com>
2024-01-08 23:52:05 +0530

    fix functionality: yt-thumbnail-downloader

--- javascript/02-javascript-dom/07-youtube-thumbnail-downloader-app/assets/js/main.js
+++ javascript/02-javascript-dom/07-youtube-thumbnail-downloader-app/assets/js/main.js
@@ -1,1 +1,26 @@
 document.body.classList.add("bg-red-500");
+
+const inputEl = document.querySelector("input");
+const getThumbnailEl = document.querySelector("#getThumbnail");
+const imgEl = document.querySelector("img");
+
+getThumbnailEl.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const url = new URL(inputEl.value);
+  console.log(url);
+  const videoId = url.searchParams.get("v");
+
+  if (url.host == "www.youtube.com") {
+    imgEl.src = `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`;
+  } else {
+    inputEl.nextElementSibling.innerText =
+      "please past Youtube video url from search bar ";
+
+    setTimeout(() => {
+      inputEl.nextElementSibling.innerText = "";
+    }, 2000);
+  }
+
+  inputEl.value = "";
+});
