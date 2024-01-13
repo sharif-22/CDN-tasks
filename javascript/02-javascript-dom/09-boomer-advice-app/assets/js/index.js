c7bfc430a1ba4f65b8085ae2e563be7f16569144
kajasharif31 <kajasharif366@gmail.com>
2024-01-13 13:11:38 +0530

    feat: ui and logics

--- javascript/02-javascript-dom/09-boomer-advice-app/assets/js/index.js
+++ javascript/02-javascript-dom/09-boomer-advice-app/assets/js/index.js
@@ -1,1 +1,25 @@
-document.querySelector("body").classList.add("bg-yellow-500");
+const adviceEl = document.querySelector("#advice");
+const API_URL = "https://api.adviceslip.com/advice";
+const refreshBtnEl = document.querySelector(".refreshBtn");
+const adviceCardEl = document.querySelector(".advice-card");
+
+async function getAdvices() {
+  try {
+    const response = await fetch(API_URL);
+    const jsonData = await response.json();
+    return jsonData;
+  } catch (error) {
+    console.error("Some network error: ", error);
+  }
+}
+
+const adviceData = await getAdvices();
+
+adviceEl.textContent = adviceData.slip.advice;
+
+refreshBtnEl.addEventListener("click", () => {
+  getAdvices().then((newAdvice) => {
+    console.log(newAdvice);
+    adviceEl.textContent = newAdvice.slip.advice;
+  });
+});
