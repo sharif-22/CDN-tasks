322beae8f3fe203df2b418015e84f8f0f63b3968
kajasharif31 <kajasharif366@gmail.com>
2024-01-17 22:49:32 +0530

    fix : intial ui

--- javascript/02-javascript-dom/11-split-calculator-app/assets/js/index.js
+++ javascript/02-javascript-dom/11-split-calculator-app/assets/js/index.js
@@ -1,3 +1,71 @@
 import countryData from "../../data/country.json";
+import numberFormatter from "number-formatter";
 
-console.log(countryData);
+// DOM
+const calculatorFormEl = document.querySelector(".calculatorForm");
+const saveTransactionEl = document.querySelector(".saveTransaction");
+
+const splitAmountFormEl = document.forms.splitAmountForm;
+const currencySelectEl = document.getElementById("currency");
+const rangeEl = document.querySelector(".range");
+const splitNumElArr = document.querySelectorAll(".splitNum");
+
+const totalExpenceEl = document.querySelector(".total-expence");
+const eachOnePayEl = document.querySelector(".eachOnePays");
+
+// event listener
+splitAmountFormEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  swapUI(calculatorFormEl, saveTransactionEl);
+
+  const formData = new FormData(splitAmountFormEl);
+  const formObj = Object.fromEntries(formData.entries());
+
+  const currentCountry = countryData.filter((symbol) => {
+    return formObj.currency === symbol.currency.code;
+  });
+  const symbol = currentCountry[0].currency.symbol;
+
+  totalExpenceEl.textContent = `${symbol}${numberFormatter(
+    "#,##0.####",
+    formObj.amount
+  )}`;
+
+  eachOnePayEl.textContent = `${symbol} ${splitAmount(
+    formObj.amount,
+    formObj.splitBy
+  )}`;
+
+  splitAmountFormEl.reset();
+});
+
+rangeEl.addEventListener("change", (e) => {
+  splitNumElArr.forEach((members) => (members.textContent = e.target.value));
+});
+
+countryData.forEach((data) => {
+  const countryCurrency = data.currency;
+  const optgroupEl = document.createElement("optgroup");
+  const optEl = document.createElement("option");
+
+  optEl.classList.add("p-2", "m-2");
+  optgroupEl.setAttribute(
+    "label",
+    `${countryCurrency.code} : ${countryCurrency.symbol}`
+  );
+  optEl.setAttribute("value", countryCurrency.code);
+  optEl.textContent = countryCurrency.name;
+
+  optgroupEl.append(optEl);
+  currencySelectEl.append(optgroupEl);
+});
+
+function splitAmount(amount, splitBy) {
+  return numberFormatter("#,##0.####", amount / splitBy);
+}
+
+function swapUI(fromEl, toEl) {
+  fromEl.classList.add("hidden");
+  toEl.classList.remove("hidden");
+}
--- javascript/02-javascript-dom/11-split-calculator-app/tailwind.config.js
+++ javascript/02-javascript-dom/11-split-calculator-app/tailwind.config.js
@@ -5,13 +5,24 @@ export default {
     extend: {
       colors: {
         primary: {
-          100: "#C1CED9",
-          200: "#7B92A6",
-          300: "#405F73",
-          400: "#011126",
+          white: "#F2F2F2",
+          black: "#262626",
+          100: "#F25C05",
+          200: "#F24405",
+          300: "#8C3908",
         },
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "light", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 };
