f0bab5089868e952d4ba9ae33d91315015831709
kajasharif31 <kajasharif366@gmail.com>
2024-02-08 22:53:54 +0530

    feat:submit alert

--- react/06-react-forms/src/components/Forms/Input.jsx
+++ react/06-react-forms/src/components/Forms/Input.jsx
@@ -14,7 +14,7 @@ const Input = ({
     <div className={`flex flex-col p-3 gap-y-2 ${width}`}>
       <div>
         <label htmlFor={name}>{label}</label>
-        {required ? <span className="text-red-500 font-bold">*</span> : ""}
+        {required ? <span className="text-red-500 font-bold px-1">*</span> : ""}
       </div>
       <input
         className="outline-none rounded bg-slate-200 p-2"
--- react/06-react-forms/src/pages/Contact.jsx
+++ react/06-react-forms/src/pages/Contact.jsx
@@ -1,31 +1,71 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Input from "../components/Forms/Input";
+import TextAreaInput from "../components/Forms/TextAreaInput";
 import ButtonInput from "../components/Forms/ButtonInput";
+import { SlClose } from "react-icons/sl";
+
 const Contact = () => {
   const [formData, setFormData] = useState({
     fName: "",
     lName: "",
     phNum: "",
     mail: "",
+    desc: "",
+  });
+  const [alertHidden, setAlertHidden] = useState({
+    statues: false,
+    classes: "alert-pop-up",
   });
 
-  const handleInputs = (e) => {
+  const handleInputs = (e, clear) => {
     const { name, value } = e.target;
     setFormData((prevState) => ({
       ...prevState, //spreading the prevState obj
       [name]: value, // if the key matches we re-assign with new value or else we add new property
     }));
+
+    clear ? setFormData(clearForm) : "";
   };
   const submitFormToServer = (e) => {
     e.preventDefault();
     console.log(formData);
-    alert(`Form submitted, successfully! ${formData.fName}`);
+
+    //rubber animation
+    setTimeout(() => {
+      setAlertHidden((prevState) => ({
+        ...prevState,
+        classes: "alert-pop-up transition-all ease-in scale-x-150 duration-300",
+      }));
+    }, 100);
+    setInterval(() => {
+      setAlertHidden((prevState) => ({
+        ...prevState,
+        classes: "alert-pop-up transition-all ease-in scale-100 duration-300",
+      }));
+    }, 300);
+
+    // alerting username
+    setAlertHidden((prevState) => ({
+      ...prevState,
+      statues: true,
+    }));
+  };
+
+  const handleAlert = () => {
+    setAlertHidden((prevState) => ({
+      ...prevState,
+      classes: "alert-pop-up",
+      statues: false,
+    }));
+
+    // reset form
     setFormData({
       fName: "",
       lName: "",
       phNum: "",
       mail: "",
+      desc: "",
     });
   };
 
@@ -34,10 +74,10 @@ const Contact = () => {
       <Navbar />
       <form
         onSubmit={submitFormToServer}
-        className="bg-slate-300 mx-auto p-3 rounded max-w-lg my-32"
+        className="bg-slate-100 mx-auto  p-3 rounded max-w-lg my-32 m-2"
       >
         {/* name */}
-        <div className="flex w-full">
+        <div className="flex w-full flex-col md:flex-row">
           <Input
             width={"w-full"}
             type="Text"
@@ -82,12 +122,33 @@ const Contact = () => {
           handleOnChange={handleInputs}
         />
         {/* Submit */}
+        <TextAreaInput
+          handleOnChange={handleInputs}
+          name="desc"
+          row={3}
+          value={formData.desc}
+          placeholder="Write detailed description"
+        />
         <ButtonInput
           bgColor="bg-green-400"
           bgColorHover="hover:bg-green-500"
           btnName={"Submit"}
         />
       </form>
+
+      <div className={alertHidden.statues ? alertHidden.classes : "hidden"}>
+        <p className="text-lg text-center">
+          Thank's for your response{" "}
+          <span className="font-medium">
+            {formData.fName + " " + formData.lName}
+          </span>
+        </p>
+        <SlClose
+          onClick={handleAlert}
+          size={32}
+          className="text-slate-700 md:rounded-full md:p-0.5 md:bg-slate-300 cursor-pointer md:hover:bg-slate-200 transition-all hover:-rotate-180 duration-700 hover:scale-125"
+        />
+      </div>
     </>
   );
 };
