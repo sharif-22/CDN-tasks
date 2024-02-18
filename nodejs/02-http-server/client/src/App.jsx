1a1f07785ad09c469b96f989d820b6d1e1097dfe
kajasharif31 <kajasharif366@gmail.com>
2024-02-18 23:00:14 +0530

    feat : connected backend api with frontend ui

--- nodejs/02-http-server/client/src/App.jsx
+++ nodejs/02-http-server/client/src/App.jsx
@@ -1,15 +1,35 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import InternsCard from "./components/InternsCard";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [internData, setInternData] = useState([]);
+
+  const fetchInterns = async () => {
+    const data = await fetch("/api");
+
+    const dataJson = await data.json();
+
+    setInternData(dataJson);
+
+    return dataJson;
+  };
+
+  useEffect(() => {
+    fetchInterns();
+  }, []);
+  // console.log(internData);
+
   return (
     <>
       <Navbar />
       <div className="max-w-7xl mx-auto grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 items-center justify-center">
-        {data.map((intern) => {
-          return <InternsCard github={intern.github} name={intern.name} />;
-        })}
+        {internData.length != 0
+          ? internData.map((intern) => {
+              return <InternsCard github={intern.github} name={intern.name} />;
+            })
+          : []}
       </div>
       <Footer />
     </>
--- nodejs/02-http-server/server/index.js
+++ nodejs/02-http-server/server/index.js
@@ -4,17 +4,9 @@ import internsData from "./data.json" assert { type: "json" };
 const SERVER_PORT = "1234";
 
 const server = http.createServer((request, response) => {
-  if (request.url === "/interns") {
-    console.log("Server is running on Port: " + SERVER_PORT);
-    response.write(JSON.stringify(internsData));
-  } else {
-    response.write(
-      JSON.stringify({
-        message: "simple react + nodeJS app",
-        routes: [{ interns: "/interns" }],
-      })
-    );
-  }
+  console.log("Server is running on Port: " + SERVER_PORT);
+  response.write(JSON.stringify(internsData));
+
   response.end();
 });
 
