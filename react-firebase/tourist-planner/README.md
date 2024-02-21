9c6a1d341471e6bed8278fd17acdd4b91fd1a7e5
kajasharif31 <kajasharif366@gmail.com>
2024-02-21 15:45:24 +0530

    intial : React + tailwind setup

---
+++ react-firebase/tourist-planner/README.md
@@ -0,0 +1,10 @@
+# Tourist planner
+
+### TODO
+
+#### pages
+
+- [ ] home pages
+- [ ] add Trip
+- [ ] view Past Trips
+- [ ] past Trip Details page
---
+++ react-firebase/tourist-planner/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className="bg-slate-400 p-4">RoamRover</div>;
+};
+
+export default App;
